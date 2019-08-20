217c34fdee8efd555636097efdaf5a085cda099f
Jackson Tubbs <decknot@Jacksons-MacBook-Pro.local>
2019-08-20 13:37:36 -0600

    Initial Commit

---
+++ deckOfCardsObjectiveC/Resources/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  deckOfCardsObjectiveC
+//
+//  Created by Jackson Tubbs on 8/20/19.
+//  Copyright © 2019 Jax Tubbs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ deckOfCardsObjectiveC/Resources/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  deckOfCardsObjectiveC
+//
+//  Created by Jackson Tubbs on 8/20/19.
+//  Copyright © 2019 Jax Tubbs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
